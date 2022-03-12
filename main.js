ffbeacf210d9a62d7798b08560e4ae0c68511e7f
R1senDev <skip.py@mail.ru>
2022-03-12 13:51:21 +0300

    Update main.js

--- main.js
+++ main.js
@@ -18,12 +18,38 @@ raccoonSprites.openMouthBlinking.src = 'openMouthBlinking.png';
 raccoonSprites.screaming.src = 'screaming.png';
 raccoonSprites.screamingBlinking.src = 'screamingBlinking.png';
 
+let raccoon = {
+	isBlinking: false,
+	mouthState: 0,
+}
+
+let blinkingIntervalValue = 3000;
+let blinkingTime = 50;
+let blinkingInterval = setInterval(function() {
+	raccoon.isBlinking = true;
+	setTimeout(function() {
+		raccoon.isBlinking = false;
+	}, blinkingTime);
+}, blinkingIntervalValue + blinkingTime);
+let volume = {
+	current: 0,
+	zero: 1,
+	speaking: 50,
+	screaming:75,
+};
+
 function redraw() {
 	context.fillStyle = backgroundColor;
 	context.fillRect(0, 0, canvas.width, canvas.height);
+	if (volume.current <= volume.zero) {
+		if (raccoon.isBlinking) {
+			//context.drawImage(raccoonSprites.blinking, 0, 0, , , 0, 0, , );
+		} else {
+			//context.drawImage(raccoonSprites.idle, 0, 0, , , 0, 0, , );
+		}
+	}
 }
 
-let currentVolume = 0;
 navigator.mediaDevices.getUserMedia({
 	audio: true
 })
@@ -44,7 +70,7 @@ navigator.mediaDevices.getUserMedia({
 		analyser.getByteFrequencyData(array);
 		const arraySum = array.reduce((a, value) => a + value, 0);
 		const average = arraySum / array.length;
-		currentVolume = Math.round(average);
+		volume.current = Math.round(average);
 	};
 })
 .catch(function(err) {
